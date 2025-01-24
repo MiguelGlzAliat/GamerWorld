2f4c4607d4cd9d144b373c8c8ed7d2cca763b157
MiguelGlzAliat <miguel.gonzalez@redaliat.mx>
2025-01-23 23:05:36 -0600

    Final Menu con secciones e iconos

--- GamerWorld/MainPage.xaml.cs
+++ GamerWorld/MainPage.xaml.cs
@@ -14,9 +14,9 @@
             count++;
 
             if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+                CounterBtn.Text = $"Clickeado {count} vez";
             else
-                CounterBtn.Text = $"Clicked {count} times";
+                CounterBtn.Text = $"Clickeado {count} veces";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
